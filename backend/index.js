49fae254e4d009b41f39ad277f61a06d016a722d
alameentar <mrtaura001@gmail.com>
2026-05-12 00:47:00 +0100

    fix braintree gateway init

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
 // BRAINTREE
 // ─────────────────────────────────────────
 const braintree = require('braintree');
-const btGateway = braintree.connect({
+const btGateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
   publicKey:  process.env.BRAINTREE_PUBLIC_KEY,
